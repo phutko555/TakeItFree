13d747bbc248694209cf8cb16384cefd86790515
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-09-01 20:07:31 +0400

    Stage2Updated

--- TakeItFree/src/main/java/com/TakeItFree/TakeItFree/Repositories/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.TakeItFree.TakeItFree.Repositories;
-
-
-import com.TakeItFree.TakeItFree.Security.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
--- TakeItFree/src/main/java/com/TakeItFree/TakeItFree/Repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.TakeItFree.TakeItFree.Repositories;
-
-import com.TakeItFree.TakeItFree.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-}

ccff3df18aaceb2d6ea9d0760bf529b23348c590
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-09-03 03:47:33 +0400

    Stage4

--- TakeItFree copy/src/main/java/com/takeItFree/takeItFree/repositories/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.takeItFree.takeItFree.repositories;
-
-
-import com.takeItFree.takeItFree.security.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}

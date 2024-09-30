c41d2be6bc0284a728c92ea624c3768d1e6e30ef
Giorgi Phutkaradze <phutkaradzeg3@gmail.com>
2024-09-30 16:14:29 +0400

    newone

--- TakeItFree copy 6/src/main/java/com/takeItFree/takeItFree/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.takeItFree.takeItFree.repositories;
-
-import com.takeItFree.takeItFree.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-}
